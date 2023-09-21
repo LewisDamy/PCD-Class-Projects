5472ac4535bb096636ddc85c44db53a8ee9fabae
LewisDamy <ldamy16@gmail.com>
2023-09-21 14:39:44 -0300

    feat: basic logic getNeighbors done

--- main.c
+++ main.c
@@ -39,8 +39,8 @@ void initGrid(float* grid[DIMENSION][DIMENSION]) {
     *grid[line + 2][column + 1] = 1.0;
 }
 
-void displayGrid(float* grid[DIMENSION][DIMENSION], char* fileName) {
-    FILE* file = fopen(fileName, "w");
+void displayGrid(float* grid[DIMENSION][DIMENSION]) {
+    FILE* file = fopen("display.txt", "w");
 
     if (file == NULL) {
         printf("Error opening the file.\n");
@@ -82,12 +82,43 @@ void copyCopyToOriginal() {
     }
 }
 
+void printCell(float* whichGrid[DIMENSION][DIMENSION], int i, int j) {
+    printf("cell value: %0.1f\n", *whichGrid[0][1]);
+}
+
+int getNeighbors(float* whichGrid[DIMENSION][DIMENSION], int i, int j) {
+    float arrNeighbors[8];
+    int amountLivingNeighbors = 0;
+
+    if (i == 0 || j == 0) {
+        printf("TODO\n");
+    } else {
+        arrNeighbors[0] = *whichGrid[i - 1][j - 1];  // TOP LEFT ITEM
+        arrNeighbors[1] = *whichGrid[i - 1][j];      // TOP MIDDLE ITEM
+        arrNeighbors[2] = *whichGrid[i - 1][j + 1];  // TOP RIGHT ITEM
+        arrNeighbors[3] = *whichGrid[i][j - 1];      // LEFT ITEM
+        arrNeighbors[4] = *whichGrid[i][j + 1];      // RIGHT ITEM
+        arrNeighbors[5] = *whichGrid[i + 1][j - 1];  // BOTTOM LEFT ITEM
+        arrNeighbors[6] = *whichGrid[i + 1][j];      // BOTTOM MIDDLE ITEM
+        arrNeighbors[7] = *whichGrid[i + 1][j + 1];  // BOTTOM RIGHT ITEM
+    }
+    // loop to make the math
+    for (int i = 0; i < 8; i++) {
+        amountLivingNeighbors += arrNeighbors[i];
+    }
+    return amountLivingNeighbors;
+}
+
 int main() {
     // Initialize the original and copy grids
     initGrid(originalGrid);
     initGrid(copyGrid);
 
+    // printf("cell value: %f\n", *originalGrid[0][1]);
+
     // TODO
+    int result = getNeighbors(originalGrid, 0, 1);
+    printf("result: %i\n", result);
 
     return 0;
 }
